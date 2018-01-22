13a3fbe9b5b02fc6c61a2c1a10543c84492e90e9
Dany-Nok <daniela_gallardo_m@hotmail.com>
2018-01-22 12:36:49 -0300

    Subiendo test calculadora

---
+++ src/calculator.js
@@ -0,0 +1,22 @@
+objCalculator = {
+  addNumber: function(a, b) {
+    return a + b;
+  },
+
+  substractNumber: function(a, b) {
+    return a - b;
+  },
+
+  multiplyNumber: function(a, b) {
+    return a * b;
+  },
+
+  divideNumber: function(a, b) {
+    return a / b;
+  }
+
+}
+
+if (typeof module != 'undefined' && module.exports) {
+  module.exports = objCalculator;
+}
